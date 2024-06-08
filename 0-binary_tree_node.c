761d60c78dab0a292ccd8b9bb252334736ebdc8e
Makenalyn <makenalynton@gmail.com>
2024-06-08 14:08:43 -0700

    creates a binary node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,10 +10,17 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
+	binary_tree_t *newnode;
+
+	newnode = malloc(sizeof(binary_tree_t));
+
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
 
 	newnode->n = value;
-	newnode = parent;
+	newnode->parent = parent;
 
 	return (newnode);
 }
